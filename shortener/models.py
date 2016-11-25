7440c076902e369dacf52230f6adc6798a9abf95
Mark Sia Hansheng <mark.siah@oceannexus.my>
2016-11-25 20:58:37 +0800

    Added code generator function to shotener app

--- shortener/models.py
+++ shortener/models.py
@@ -1,6 +1,18 @@
+import random
+import string
+
 from django.db import models
 
 # Create your models here.
+
+
+def code_generator(size=6, chars=string.ascii_lowercase + string.digits):
+    # new_code = ''
+    # for _ in range(size):
+    #     new_code += random.choice(chars)
+    # return new_code
+    return ''.join(random.choice(chars) for _ in range(size))
+
 
 
 class KirrURL(models.Model):
@@ -14,3 +26,9 @@ class KirrURL(models.Model):
 
     def __str__(self):
         return str(self.url)
+
+    def save(self, *args, **kwargs):
+        print("something")
+        self.shortcode = code_generator()
+        super(KirrURL, self).save(*args, **kwargs)
+
