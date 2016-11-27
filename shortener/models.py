6f7ae63964be73d62a77e27dac3cae9a0743f793
Mark Sia Hansheng <mark.siah91@gmail.com>
2016-11-27 18:11:11 +0800

    Added utils.py

--- shortener/models.py
+++ shortener/models.py
@@ -3,20 +3,12 @@ import string
 
 from django.db import models
 
+from .utils import code_generator, create_shortcode
 # Create your models here.
-
-
-def code_generator(size=6, chars=string.ascii_lowercase + string.digits):
-    # new_code = ''
-    # for _ in range(size):
-    #     new_code += random.choice(chars)
-    # return new_code
-    return ''.join(random.choice(chars) for _ in range(size))
-
 
 class KirrURL(models.Model):
     url = models.CharField(max_length=220,)
-    shortcode = models.CharField(max_length=15, unique=True)
+    shortcode = models.CharField(max_length=15, unique=True, blank=True)
     timestamp = models.DateTimeField(auto_now=True) # when model was created
     updated = models.DateTimeField(auto_now_add=True) # everytime the model is saved
     # empty_datetime = models.DateTimeField(auto_now=False, auto_now_add=False)
@@ -27,7 +19,7 @@ class KirrURL(models.Model):
         return str(self.url)
 
     def save(self, *args, **kwargs):
-        print("something")
-        self.shortcode = code_generator()
+        if self.shortcode is None or self.shortcode == "":
+            self.shortcode = create_shortcode(self)
         super(KirrURL, self).save(*args, **kwargs)
 
